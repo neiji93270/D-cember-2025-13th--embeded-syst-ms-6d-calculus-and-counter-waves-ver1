936eb1b2ab844fc2e150c3afc00e732a87fb6be2
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-12-14 06:38:20 +0100

    Create ciphersfromintegralspropertiesDNotDummyScaleOfIntegrals.h

---
+++ ciphersfromintegralspropertiesDNotDummyScaleOfIntegrals.h
@@ -0,0 +1,5 @@
+//The ciphers for embeded systems that carry hidden machine internal web must rely on different topologies of pro p groups.
+///Then, this is intuitive to thinl about a pre processor of ciphers communcating with devices. To overcome OS usage,
+//we can omit large scale algebraic topologies values. So we rely on infinite ticks of chips and modulus of a certain pro p group.
+//Then, it is best to hide topologies sniffed by attackers. To do that, we can use dummy signals to detect intruders and that said,
+//we cannot omit signals scattering in a splatted gaussian heat of chips. So lets end messages of signals in 6d !
